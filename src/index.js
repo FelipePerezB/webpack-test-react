061aed91ff72fa84d373c02238f7ca37872eeffe
felipe <felipeeperez3@gmail.com>
2022-02-14 00:04:17 -0300

    v1

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App.jsx"
+import "./styles/style.css"
+
+ReactDom.render(<App />,document.getElementById("app"))
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
+
+
+module.exports={
+    mode:"production",
+    entry:"./src/index.js",
+    output:{
+        path:path.resolve(__dirname,"dist"),
+        filename:"main.js",
+        clean:true,
+        publicPath:"/"
+    },
+    resolve:{
+        extensions:[".js",".jsx"],
+        alias:{
+            "@styles":path.resolve(__dirname,"src/styles/"),
+            "@components":path.resolve(__dirname,"src/components/")
+        }
+    },
+    module:{
+        rules:[
+            {
+                test:/\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:"babel-loader"
+                }
+            },
+            {
+                test:/\.html$/,
+                use:[
+                    {loader:"html-loader"}
+                ]
+            },
+            {
+                test:/\.css$/i,
+                use:[
+                    MiniCssExtractPlugin.loader,"css-loader"
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            inject:true,
+            template:"./public/index.html",
+            filename:"./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename:"[name].css",
+        })
+    ],
+    optimization:{
+        minimize:true,
+        minimizer:[
+            new TerserPlugin(),
+            new CssMinimizerPlugin()
+        ]
+    },
+}
